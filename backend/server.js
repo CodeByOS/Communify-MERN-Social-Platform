b07a120c44ddc8e316d8100df446bef16e3abdef
Oussama <saidi.oussama2004@gmail.com>
2025-05-12 23:39:32 +0100

    Initial MERN APP Configuration/Setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//* Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+//* Define Routes
+
+//* Start the MongoDB connection and then the server
+connectDB()
+    .then(() => {
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+        process.exit(1);
+    });
