a8288819825be6d4f606294b552abee017b639f6
Oussama <saidi.oussama2004@gmail.com>
2025-05-12 23:42:23 +0100

    Connect MongoDB with express Server..

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const connectDB = require("./config/db");
 const app = express();
 
 //* Middlewares
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 app.use(bodyParser.json());
 
 //* Define Routes
