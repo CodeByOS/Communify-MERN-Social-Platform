3aa1aee1ab1858ab3ec7a9ed5c55d630af5eef49
Oussama <saidi.oussama2004@gmail.com>
2025-05-13 22:35:22 +0100

    Add check user Routes & Logic..

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,9 +4,14 @@ const protectRoute = require("../middlewares/authMiddlewares");
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", logout); //* POST METHOD --> For Updating something (jwt in this example) in server side
 
 router.post("/onboarding", protectRoute, onboard);
+
+//* Check if user is logged in
+router.get("/me", protectRoute, (req, res) => {
+    res.status(200).json({ success: true, user: req.user })
+})
 
 module.exports = router;
 
