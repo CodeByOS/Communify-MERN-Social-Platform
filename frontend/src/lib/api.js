7b7a67fdae34d6168c6de774be77adcce4202988
Oussama <saidi.oussama2004@gmail.com>
2025-05-14 23:18:42 +0100

    Finish LoginPage Component + Modify All the routes logic..

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -5,6 +5,11 @@ export const signupFn = async (signupData) => {
     return res.data;
 }
 
+export const loginFn = async (loginData) => {
+    const res = await axiosInstance.post("/auth/login", loginData);
+    return res.data;
+}
+
 export const getAuthUser = async() => {
     const res = await axiosInstance.get("/auth/me");
     return res.data;
