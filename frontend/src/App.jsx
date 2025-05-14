fb3dad91c840ac932568d35a2539887c4716dff2
Oussama <saidi.oussama2004@gmail.com>
2025-05-14 11:19:29 +0100

    Add React Toast notifications and Test it..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,36 @@
+import { Route, Routes } from "react-router";
+
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import OnboargingPage from "./pages/OnboardingPage.jsx"
+
+import toast, { Toaster } from "react-hot-toast"
+
+
+
 const App = () => {
   return (
-    <div className="h-screen p-5 text-white" data-theme="nigth">
-      Hello World
+    <div className="h-screen" data-theme="night">
+
+      <button onClick={() => toast.success("GOOD") }>Hello World</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboargingPage />} />
+        
+      </Routes>
+
+
+      <Toaster />
     </div>
   )
 }
