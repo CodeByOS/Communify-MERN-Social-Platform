1deade43422bbccabe6b65505fbd444e7d241fb4
Oussama <saidi.oussama2004@gmail.com>
2025-05-15 12:17:42 +0100

    Update zustand for manage Theme..

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-    theme: localStorage.getItem("streamify-theme") || "night",
+    theme: localStorage.getItem("communify-theme") || "night",
     setTheme: (theme) => {
-        localStorage.setItem("streamify-theme", theme);
+        localStorage.setItem("communify-theme", theme);
         set({ theme });
     },
 }));
